142fea45dcffc164a6e11a0cf90fea7243e76204
An minsun <elleto123456@gmail.com>
2025-01-21 23:28:31 +0900

    게시글(감상글) 리스트

---
+++ app/src/main/java/com/example/moviefilmproject/CommentModel.kt
@@ -0,0 +1,9 @@
+package com.example.moviefilmproject
+
+data class CommentModel(
+    val movieTitle: String = "",
+    val title: String = "",
+    val content: String = "",
+    val time: String = ""
+)
+
